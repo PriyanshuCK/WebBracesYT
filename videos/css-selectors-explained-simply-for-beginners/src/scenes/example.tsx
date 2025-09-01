651c8d11f8ba25ecc486f5887337ffaa7021a432
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-09-01 10:41:59 +0530

    Test changing circle color in example scene

--- videos/css-selectors-explained-simply-for-beginners/src/scenes/example.tsx
+++ videos/css-selectors-explained-simply-for-beginners/src/scenes/example.tsx
@@ -1,12 +1,11 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {createRef} from '@motion-canvas/core';
+import { Circle, makeScene2D } from '@motion-canvas/2d';
+import { createRef } from '@motion-canvas/core';
+import colors from '../lib/colors';
 
-export default makeScene2D(function* (view) {
-  // Create your animations here
+export default makeScene2D(function*(view) {
+	const circle = createRef<Circle>();
 
-  const circle = createRef<Circle>();
+	view.add(<Circle ref={circle} size={320} fill={colors.green[500]} />);
 
-  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
-
-  yield* circle().scale(2, 2).to(1, 2);
+	yield* circle().scale(2, 2).to(1, 2);
 });
