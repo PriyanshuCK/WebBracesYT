ce8906e62ccd8196f6de82c27ef5b163cffb2ed4
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-11-05 11:41:36 +0530

    WIP: scene-4

--- videos/css-selectors-explained-simply-for-beginners/src/project.ts
+++ videos/css-selectors-explained-simply-for-beginners/src/project.ts
@@ -5,8 +5,9 @@ import scene1 from "./scenes/scene1-intro?scene";
 import scene2 from "./scenes/scene2-type-class-intro?scene";
 import scene3 from "./scenes/scene3-multiple-classes?scene";
 import scene4 from "./scenes/scene4-case-sensitivity-&-invalidity?scene";
+import scene5 from "./scenes/scene5-ID-intro-syntax-&-specificity?scene";
 
 export default makeProject({
-  scenes: [scene1, scene2, scene3, scene4],
+  scenes: [scene1, scene2, scene3, scene4, scene5],
   audio: audioAI1,
 });
--- videos/css-selectors-explained-simply-for-beginners/src/scenes/scene4-case-sensitivity-&-invalidity.tsx
+++ videos/css-selectors-explained-simply-for-beginners/src/scenes/scene4-case-sensitivity-&-invalidity.tsx
@@ -1,15 +1,171 @@
-import { makeScene2D } from "@motion-canvas/2d";
+import { Code, Layout, makeScene2D } from "@motion-canvas/2d";
 import colors from "../lib/colors";
-import { Grid } from "../nodes";
-import { Direction, slideTransition, waitUntil } from "@motion-canvas/core";
+import { Cursor, ExtendedRect, Grid, ViewportManager } from "../nodes";
+import { all, createRef, Direction, Reference, slideTransition, waitUntil } from "@motion-canvas/core";
+import { ExtendedTxt } from "../nodes/ExtendedTxt";
+import { spaceNX, spaceNY, spaceY } from "../lib/space";
 
 export default makeScene2D(function*(view) {
-	view.fontFamily('Geist');
-	view.fill(colors.zinc[950]);
-	view.opacity(0.3);
-	view.add(<Grid />);
+  view.fontFamily('Geist');
+  view.fill(colors.zinc[950]);
+  view.opacity(0.3);
+  view.add(<Grid />);
 
-	yield* slideTransition(Direction.Right, 0.75);
+  yield* slideTransition(Direction.Right, 0.75);
 
-	yield* waitUntil("s4-end");
+  const doubtEmoji = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <ExtendedTxt
+        ref={doubtEmoji}
+        text={"🤔"}
+        fontSize={spaceY[1]}
+        position={[0, spaceY[1]]}
+        opacity={0}
+        rotation={-15}
+      />
+    </>
+  );
+
+  yield* all(
+    doubtEmoji().opacity(1, 0.75),
+    doubtEmoji().y(0, 0.75),
+    doubtEmoji().rotation(0, 0.75),
+  );
+
+  yield* waitUntil("are-case-sensitive");
+  const areClassSelectorsCaseSensitive = createRef<ExtendedTxt>();
+  const threeSelectorsLayout = createRef<Layout>();
+  const selectorsHighlight = createRef<ExtendedRect>();
+  const sameOrDifferentTxt = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <ExtendedTxt
+        ref={areClassSelectorsCaseSensitive}
+        fontSize={spaceY[0.75]}
+        opacity={0}
+        y={spaceNY[3.5]}
+      />
+      <Layout
+        ref={threeSelectorsLayout}
+        layout
+        direction={"column"}
+        gap={spaceY[0.25]}
+        opacity={0}
+      >
+        <ExtendedTxt text={".myclass"} fontSize={spaceY[0.5]} />
+        <ExtendedTxt text={".MyClass"} fontSize={spaceY[0.5]} />
+        <ExtendedTxt text={".MYCLASS"} fontSize={spaceY[0.5]} />
+      </Layout>
+      <ExtendedRect
+        ref={selectorsHighlight}
+        size={0}
+        position={[spaceNX[1.5], spaceNY[1.5]]}
+        highlighted
+        opacity={0}
+      />
+      <ExtendedTxt
+        ref={sameOrDifferentTxt}
+        y={spaceY[3.5]}
+        opacity={0}
+        fontSize={spaceY[0.5]}
+      />
+    </>
+  );
+
+  yield* all(
+    doubtEmoji().y(spaceY[1], 0.75),
+    doubtEmoji().opacity(0, 0.75),
+    doubtEmoji().rotation(15, 0.75),
+    areClassSelectorsCaseSensitive().opacity(1, 0.75),
+    areClassSelectorsCaseSensitive().text("Are class selectors", 1.5),
+    threeSelectorsLayout().opacity(1, 0.75),
+    selectorsHighlight().size([spaceY[3], spaceY[3]], 0.75),
+    selectorsHighlight().position(0, 0.75),
+    selectorsHighlight().opacity(1, 0.75),
+  );
+
+  yield* all(
+    areClassSelectorsCaseSensitive().text("Are class selectors case-sensitive?", 1),
+    sameOrDifferentTxt().text("same or different? 🤔", 1),
+    sameOrDifferentTxt().opacity(1, 0.75),
+    sameOrDifferentTxt().y(spaceY[2.5], 0.75),
+  );
+
+  yield* waitUntil("different");
+  yield* sameOrDifferentTxt().text("Different!", 0.75);
+
+  yield* waitUntil("whenWorkingWithHTML");
+  const workingWithHTMLTxt = createRef<ExtendedTxt>();
+  const classNamesSensitiveTxt = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <ExtendedTxt
+        ref={workingWithHTMLTxt}
+        fontSize={spaceY[0.75]}
+      />
+      <ExtendedTxt
+        ref={classNamesSensitiveTxt}
+        fontSize={spaceY[0.75]}
+      />
+    </>
+  )
+
+  yield* all(
+    areClassSelectorsCaseSensitive().opacity(0, 0.75),
+    threeSelectorsLayout().opacity(0, 0.75),
+    selectorsHighlight().end(0, 0.75),
+    selectorsHighlight().opacity(0, 0.75),
+    sameOrDifferentTxt().opacity(0, 0.75),
+    workingWithHTMLTxt().text("When working with HTML,", 1),
+  )
+
+  yield* all(
+    workingWithHTMLTxt().fontSize(spaceY[0.5], 0.75),
+    workingWithHTMLTxt().y(spaceNY[1.5], 0.75),
+    classNamesSensitiveTxt().text("class names are case-sensitive!", 1),
+  )
+
+  yield* waitUntil("the-browser");
+
+  yield* all(
+    workingWithHTMLTxt().opacity(0, 0.5),
+    classNamesSensitiveTxt().opacity(0, 0.5),
+  );
+
+  const viewportManager = new ViewportManager()
+    .addHtml(`\
+<p class="myclass">
+  Hello World
+</p>
+<p class="MyClass">
+  Hello World
+</p>
+<p class="MYCLASS">
+  Hello World
+</p>
+`)
+    .addCss()
+    .addBrowser();
+
+  viewportManager.addToView(view);
+
+  const refs = viewportManager.getViewportRefs();
+  const htmlCode: Reference<Code> = refs.html?.code;
+  const cssCode: Reference<Code> = refs.css?.code;
+  const cssViewport = refs.css?.viewport;
+
+  yield* all(
+    slideTransition(Direction.Right, 0.75),
+    viewportManager.animateToPresetLayout('EQ_H'),
+  );
+
+  const cursor = createRef<Cursor>();
+  view.add(
+    <>
+      <Cursor ref={cursor} position={[spaceNX[4.5], spaceNY[3.5]]} opacity={0} color={"green"} />
+    </>
+  );
+
+  yield* waitUntil("s4-end");
 });
