4bbf829ea39e2dd5b127a531752ea6d43a53b533
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-11-24 10:22:13 +0530

    Add audio-v1

--- videos/css-selectors-explained-simply-for-beginners/src/project.ts
+++ videos/css-selectors-explained-simply-for-beginners/src/project.ts
@@ -1,6 +1,6 @@
 import { makeProject } from "@motion-canvas/core";
 
-import audioAI1 from "./audio/audioAI1.wav";
+import audio1 from "./audio/Final Aud1.wav";
 import scene1 from "./scenes/scene1-intro?scene";
 import scene2 from "./scenes/scene2-type-class-intro?scene";
 import scene3 from "./scenes/scene3-multiple-classes?scene";
@@ -12,5 +12,5 @@ import scene8 from "./scenes/scene8-ID-case-sensitivity-&-invalidity?scene";
 
 export default makeProject({
   scenes: [scene1, scene2, scene3, scene4, scene5, scene6, scene7, scene8],
-  audio: audioAI1,
+  audio: audio1,
 });
--- videos/css-selectors-explained-simply-for-beginners/src/scenes/scene8-ID-case-sensitivity-&-invalidity.tsx
+++ videos/css-selectors-explained-simply-for-beginners/src/scenes/scene8-ID-case-sensitivity-&-invalidity.tsx
@@ -1,7 +1,11 @@
-import { makeScene2D } from "@motion-canvas/2d";
+import { Code, lines, makeScene2D } from "@motion-canvas/2d";
 import colors from "../lib/colors";
-import { Grid } from "../nodes";
-import { Direction, slideTransition, waitUntil } from "@motion-canvas/core";
+import { Cursor, Grid, ViewportManager } from "../nodes";
+import { all, createRef, DEFAULT, delay, Direction, Reference, slideTransition, waitUntil } from "@motion-canvas/core";
+import spaceX, { spaceNX, spaceNY, spaceY } from "../lib/space";
+import { ExtendedTxt } from "../nodes/ExtendedTxt";
+import eqh1 from "../images/s8/eqh1.png";
+import eqh2 from "../images/s8/eqh2.png";
 
 export default makeScene2D(function*(view) {
   view.fontFamily('Geist');
@@ -9,7 +13,165 @@ export default makeScene2D(function*(view) {
   // view.opacity(0.7);
   view.add(<Grid />);
 
-  yield* slideTransition(Direction.Right, 0.75);
+  const viewportManager = new ViewportManager()
+    .addHtml(`\
+<p id="myclass">
+  Hello World
+</p>
+<p id="MyClass">
+  Hello World
+</p>
+<p id="MYCLASS">
+  Hello World
+</p>
+`)
+    .addCss(`\
+#myclass {
+  font-size: 1rem;
+}
+#MyClass {
+  font-size: 2rem;
+}
+#MYCLASS {
+  font-size: 3rem;
+}
+`)
+    .addBrowser(eqh1);
+
+  viewportManager.addToView(view);
+  const refs = viewportManager.getViewportRefs();
+  const htmlCode: Reference<Code> = refs.html?.code;
+  const cssCode: Reference<Code> = refs.css?.code;
+  const browserViewport = refs.browser?.viewport;
+  const wrapper = viewportManager.getWrapper();
+  viewportManager.updateLayoutDimensions(spaceX[18], spaceY[8]);
+
+  const cursor = createRef<Cursor>();
+  const title = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <Cursor ref={cursor} position={[spaceNX[5.33], spaceNY[1.5]]} opacity={0} color={"green"} />
+      <ExtendedTxt
+        ref={title}
+        fontSize={spaceY[0.5]}
+        fontWeight={500}
+        y={spaceNY[4.5]}
+      />
+    </>
+  );
+
+  yield* all(
+    wrapper().y(spaceY[1], 0.75),
+    wrapper().size([spaceX[18], spaceY[8]], 0.75),
+    slideTransition(Direction.Right, 0.75),
+    viewportManager.animateToPresetLayout('EQ_H'),
+    title().text("ID Naming Rules — Same as Classes!", 1.25),
+  );
+
+  yield* waitUntil("IDs-case-sensitive");
+  yield* all(
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[6.5], spaceNY[2.5]], 0.75).to([spaceNX[6.5], spaceY[0.33]], 0.75),
+  );
+
+  yield* waitUntil("same-naming-rules");
+  viewportManager.removeViewport("browser");
+  yield* all(
+    htmlCode().code.replace(lines(0, 8), `\
+<div id="user-name">
+  kebab-case (recommended)
+</div>
+<div id="user_name">
+  snake_case (ok)
+</div>
+<div id="userName">
+  camelCase (ok)
+</div>
+
+<div id="1st-item">
+  starts with a number (avoid!)
+  Allowed by HTML, but problematic for CSS 
+selectors
+</div>
+`, 0.75),
+    cssCode().code.replace(lines(0, 8), `\
+#user-name {
+  font-weight: 700;
+}
+#user_name {
+  font-style: italic;
+}
+#userName {
+  text-decoration: underline;
+}
+
+#\\31 st-item {
+/* escaping is error-prone and hard to read
+   avoid starting with a number */
+  color: crimson;
+}
+`, 0.75),
+    viewportManager.animateToLayout({ duration: 0.75 }),
+    cursor().opacity(0, 0.75),
+  )
+
+  yield* waitUntil("cant-start-with-number");
+  yield* all(
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[7.25] + 6, spaceY[2.25]], 0.75),
+    htmlCode().selection(lines(9, 14), 0.75),
+    cssCode().selection(lines(10, 15), 0.75),
+  )
+
+  yield* waitUntil("stick-with-letters")
+  yield* all(
+    htmlCode().selection(lines(0, 7), 0.75),
+    cssCode().selection(lines(0, 7), 0.75),
+    cursor().opacity(0, 0.75),
+  )
+
+  yield* waitUntil("hyphens");
+  yield* all(
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[6.5], spaceNY[2.5]], 0.75),
+    htmlCode().selection(lines(0, 2), 0.75),
+    cssCode().selection(lines(0, 2), 0.75),
+  )
+
+  yield* waitUntil("underscores");
+  yield* all(
+    cursor().position([spaceNX[6.5], spaceNY[1]], 0.75),
+    htmlCode().selection(lines(3, 5), 0.75),
+    cssCode().selection(lines(3, 5), 0.75),
+  )
+
+  yield* waitUntil("same-ID-twice");
+  htmlCode().selection(DEFAULT);
+  cssCode().selection(DEFAULT);
+  viewportManager.showViewport("browser");
+  yield* all(
+    viewportManager.animateToPresetLayout('EQ_H', { duration: 0.75, browserImage: eqh2 }),
+    title().text("Duplicate IDs", 1.25),
+    htmlCode().code.replace(lines(0, 14), `\
+<div id="highlight">
+  First element using #highlight
+</div>
+`, 0.75),
+    cssCode().code.replace(lines(0, 14), `\
+#highlight {
+  color: green;
+}
+`, 0.75),
+    cursor().opacity(0, 0.5),
+  )
+  yield* all(
+    htmlCode().code.append(`\
+<div id="highlight">
+  Second element using #highlight
+</div>
+`, 0.75),
+    delay(0.5, cursor().opacity(1, 0.75)),
+  );
 
   yield* waitUntil("s8-end");
 })
