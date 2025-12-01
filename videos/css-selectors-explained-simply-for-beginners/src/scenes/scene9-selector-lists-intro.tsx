9dc24f8e617a2e4922b2307d56e074ed858eef69
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-12-01 13:09:22 +0530

    WIP: scene-9

--- videos/css-selectors-explained-simply-for-beginners/src/scenes/scene9-selector-lists-intro.tsx
+++ videos/css-selectors-explained-simply-for-beginners/src/scenes/scene9-selector-lists-intro.tsx
@@ -1,14 +1,16 @@
-import { Layout, makeScene2D } from "@motion-canvas/2d";
+import { Code, Layout, lines, makeScene2D, word } from "@motion-canvas/2d";
 import colors from "../lib/colors";
 import { Cursor, Grid, ViewportManager } from "../nodes";
-import { all, createRef, delay, Direction, slideTransition, waitUntil } from "@motion-canvas/core";
+import { all, createRef, delay, Direction, Reference, slideTransition, waitUntil } from "@motion-canvas/core";
 import { ExtendedTxt } from "../nodes/ExtendedTxt";
 import spaceX, { spaceNX, spaceNY, spaceY } from "../lib/space";
+import beq1 from "../images/s9/beq1.png"
+import eqh1 from "../images/s9/eqh1.png"
 
 export default makeScene2D(function*(view) {
   view.fontFamily('Geist');
   view.fill(colors.zinc[950]);
-  // view.opacity(0.7);
+  view.opacity(0.7);
   view.add(<Grid />);
 
   const table = createRef<Layout>();
@@ -158,6 +160,243 @@ export default makeScene2D(function*(view) {
     selector3().opacity(0, 0.75),
     selects3().opacity(0, 0.75),
     delay(0.5, title().text("Selector Lists", 1)),
+  )
+
+  yield* waitUntil("instead-of")
+  yield* all(
+    title().y(spaceNY[4.5], 0.75),
+  )
+
+  yield* waitUntil("separate-rules");
+  const column1 = createRef<Layout>();
+  const column2 = createRef<Layout>();
+  const column3 = createRef<Layout>();
+  const h1 = createRef<ExtendedTxt>();
+  const h2 = createRef<ExtendedTxt>();
+  const h3 = createRef<ExtendedTxt>();
+  const arrow1 = createRef<ExtendedTxt>();
+  const arrow2 = createRef<ExtendedTxt>();
+  const arrow3 = createRef<ExtendedTxt>();
+  const blue1 = createRef<ExtendedTxt>();
+  const blue2 = createRef<ExtendedTxt>();
+  const blue3 = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <Layout
+        ref={column1}
+        layout
+        direction="column"
+        x={spaceNX[1.5]}
+        gap={spaceY[0.25]}
+      >
+        <ExtendedTxt
+          ref={h1}
+          text="h1"
+          fontSize={spaceY[0.5]}
+          opacity={0}
+          fill={"#F07178"}
+        />
+        <ExtendedTxt
+          ref={h2}
+          text="h2"
+          fontSize={spaceY[0.5]}
+          opacity={0}
+          fill={"#F07178"}
+        />
+        <ExtendedTxt
+          ref={h3}
+          text="h3"
+          fontSize={spaceY[0.5]}
+          opacity={0}
+          fill={"#F07178"}
+        />
+      </Layout>
+      <Layout
+        ref={column2}
+        layout
+        direction="column"
+        gap={spaceY[0.25]}
+      >
+        <ExtendedTxt
+          ref={arrow1}
+          text="→"
+          fontSize={spaceY[0.5]}
+          opacity={0}
+        />
+        <ExtendedTxt
+          ref={arrow2}
+          text="→"
+          fontSize={spaceY[0.5]}
+          opacity={0}
+        />
+        <ExtendedTxt
+          ref={arrow3}
+          text="→"
+          fontSize={spaceY[0.5]}
+          opacity={0}
+        />
+      </Layout>
+      <Layout
+        ref={column3}
+        layout
+        direction="column"
+        x={spaceX[1.5]}
+        gap={spaceY[0.25]}
+      >
+        <ExtendedTxt
+          ref={blue1}
+          text={"blue"}
+          fill={colors.blue[500]}
+          fontSize={spaceY[0.5]}
+          opacity={0}
+        />
+        <ExtendedTxt
+          ref={blue2}
+          text={"blue"}
+          fontSize={spaceY[0.5]}
+          fill={colors.blue[500]}
+          opacity={0}
+        />
+        <ExtendedTxt
+          ref={blue3}
+          text={"blue"}
+          fill={colors.blue[500]}
+          fontSize={spaceY[0.5]}
+          opacity={0}
+        />
+      </Layout>
+    </>
+  )
+  yield* all(
+    h1().opacity(1, 0.75),
+    arrow1().opacity(1, 0.75),
+    blue1().opacity(1, 0.75),
+    delay(0.2, h2().opacity(1, 0.75)),
+    delay(0.2, arrow2().opacity(1, 0.75)),
+    delay(0.2, blue2().opacity(1, 0.75)),
+    delay(0.4, h3().opacity(1, 0.75)),
+    delay(0.4, arrow3().opacity(1, 0.75)),
+    delay(0.4, blue3().opacity(1, 0.75)),
+  )
+
+  yield* waitUntil("group-together");
+  yield* all(
+    h1().opacity(0, 0.75),
+    arrow1().opacity(0, 0.75),
+    blue1().opacity(0, 0.75),
+    h3().opacity(0, 0.75),
+    arrow3().opacity(0, 0.75),
+    blue3().opacity(0, 0.75),
+    delay(0.25, h2().text("h1, h2, & h3", 1.25)),
+    delay(0.25, column1().x(spaceNX[2], 1.25)),
+  )
+
+  yield* waitUntil("lets-say-you-want");
+  const viewportManager = new ViewportManager()
+    .addHtml()
+    .addCss()
+    .addBrowser();
+  viewportManager.addToView(view);
+  const wrapper = viewportManager.getWrapper();
+  const refs = viewportManager.getViewportRefs();
+  const htmlCode: Reference<Code> = refs.html?.code;
+  const cssCode: Reference<Code> = refs.css?.code;
+
+  viewportManager.updateLayoutDimensions(spaceX[18], spaceY[8]);
+  viewportManager.removeViewport("css");
+  yield* all(
+    viewportManager.animateToLayout({ duration: 0.75, browserImage: beq1 }),
+    wrapper().size([spaceX[18], spaceY[8]], 0.75),
+    wrapper().y(spaceY[1], 0.75),
+    htmlCode().code.append(`\
+<h1>Main Heading</h1>
+<p>
+  Some introductory text...
+</p>
+
+<h2>Subheading</h2>
+<p>
+  Details about this section...
+</p>
+
+<h3>Section Title</h3>
+<p>
+  Additional information here...
+</p>
+`, 0.75),
+    h2().opacity(0, 0.75),
+    arrow2().opacity(0, 0.75),
+    blue2().opacity(0, 0.75),
+    title().fontSize(spaceY[0.5], 0.75),
+    title().fontWeight(500, 0.75),
+  );
+
+  yield* waitUntil("all-your-headings")
+  cursor().position([spaceNX[4], spaceNY[2]]);
+  yield* all(
+    htmlCode().selection(htmlCode().findAllRanges(/<h[1-6][^>]*>[\s\S]*?<\/h[1-6]>/g), 0.75),
+  )
+
+  yield* waitUntil("h1")
+  yield* all(
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[7], spaceNY[2.5]], 0.75),
+  )
+  yield* cursor().position([spaceNX[7.25], spaceY[0]], 0.5);
+  yield* cursor().position([spaceNX[6.75], spaceY[2.25]], 0.5);
+
+  yield* waitUntil("without-selector-lists")
+  yield* all(
+    cursor().opacity(0, 0.75),
+    cursor().position([spaceNX[4.25], spaceY[3.25]], 0.75),
+  )
+  yield* waitUntil("you-would-write-this");
+  viewportManager.showViewport("css");
+  yield* all(
+    viewportManager.animateToPresetLayout("EQ_H", { duration: 0.75, browserImage: eqh1 }),
+    cssCode().code.append(`\
+h1 {
+  color: blue;
+}
+h2 {
+  color: blue;
+}
+h3 {
+  color: blue;
+}
+`, 0.75),
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[0.5], spaceY[1.25]], 0.75),
+  )
+
+  yield* waitUntil("lots-of-repetition");
+  yield* cursor().position([spaceNX[0.5], spaceNY[1.75]], 0.75).to([spaceNX[0.5], spaceY[1.25]], 0.75);
+
+  yield* waitUntil("with-selector-lists");
+  yield* all(
+    cssCode().code.replace(lines(0, 8), `\
+h1, h2, h3 {
+  color: blue;
+}
+`, 0.75),
+    cursor().opacity(0, 0.75),
+    cursor().position([spaceX[0.5], spaceY[2.25]], 0.75),
+  )
+  yield* waitUntil("you-can-combine-them");
+  cursor().position([spaceNX[1.33], spaceNY[1.5]]);
+  yield* all(
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[2.33] + 4, spaceNY[2.5] + 8], 0.75).wait(0.2).to([spaceNX[1.67] + 8, spaceNY[2.5] + 8], 0.75),
+    cssCode().selection(word(0, 0, 10), 0.75),
+  )
+
+  yield* waitUntil("reads-as-h1")
+  yield* cursor().x(spaceNX[2.5] - 8, 0.75).to(spaceNX[1.75] - 8, 0.75).to(spaceNX[1.25], 0.75);
+
+  yield* waitUntil("should-all-blue");
+  yield* all(
+    cursor().position([spaceNX[1.33] - 8, spaceNY[2]], 0.75),
+    cssCode().selection(lines(1), 0.75),
   )
 
   yield* waitUntil("s9-end");
