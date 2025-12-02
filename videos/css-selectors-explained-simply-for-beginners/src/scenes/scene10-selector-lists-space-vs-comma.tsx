f419d3027c30423e7f21576a562056a7417f3cd2
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-12-02 12:01:09 +0530

    WIP: scene-10

--- videos/css-selectors-explained-simply-for-beginners/src/scenes/scene10-selector-lists-space-vs-comma.tsx
+++ videos/css-selectors-explained-simply-for-beginners/src/scenes/scene10-selector-lists-space-vs-comma.tsx
@@ -1,8 +1,9 @@
-import { Code, makeScene2D } from "@motion-canvas/2d";
+import { Code, makeScene2D, word } from "@motion-canvas/2d";
 import colors from "../lib/colors";
-import { Grid, ViewportManager } from "../nodes";
-import { all, Direction, Reference, slideTransition, waitUntil } from "@motion-canvas/core";
-import spaceX from "../lib/space";
+import { Cursor, Grid, ViewportManager } from "../nodes";
+import { all, createRef, Direction, Reference, slideTransition, waitUntil } from "@motion-canvas/core";
+import spaceX, { spaceNX, spaceNY } from "../lib/space";
+import eqh1 from "../images/s10/eqh1.png"
 
 export default makeScene2D(function*(view) {
   view.fontFamily('Geist');
@@ -20,8 +21,6 @@ export default makeScene2D(function*(view) {
   const refs = viewportManager.getViewportRefs();
   const htmlCode: Reference<Code> = refs.html?.code;
   const cssCode: Reference<Code> = refs.css?.code;
-  const htmlViewport = refs.html?.viewport;
-  const cssViewport = refs.css?.viewport;
   viewportManager.removeViewport("html");
   viewportManager.removeViewport("browser")
 
@@ -39,19 +38,58 @@ h1, .sidebar p {
 `, 0.75),
   )
 
-  yield* waitUntil("it-looks-like");
-  viewportManager.showViewport("html");
+  yield* waitUntil("all-h1-headings");
+  const cursor = createRef<Cursor>();
+  view.add(
+    <>
+      <Cursor ref={cursor} position={[spaceNX[3.25], spaceNY[3.5]]} opacity={0} color={"green"} />
+    </>
+  );
   yield* all(
-    viewportManager.animateToLayout({ duration: 0.75 }),
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceNX[4.25] + 8, spaceNY[4.5]], 0.75),
+  )
+
+  yield* waitUntil("sidebar-class")
+  yield* all(
+    cursor().x(spaceNX[2.75], 0.75),
+  )
+
+  yield* waitUntil("all-ps")
+  yield* all(
+    cursor().x(spaceNX[2] + 8, 0.75),
+  )
+  yield* cursor().position([spaceNX[2.33], spaceNY[4]], 0.75);
+
+  yield* waitUntil("but-actually");
+  viewportManager.showViewport("html");
+  viewportManager.showViewport("browser");
+  yield* all(
+    viewportManager.animateToPresetLayout("EQ_H", { duration: 0.75, browserImage: eqh1 }),
+    cursor().opacity(0, 0.75),
+    cursor().position([spaceX[1.33], spaceNY[3]], 0.75),
     htmlCode().code.append(`\
 <h1>Heading</h1>
 
-  <p>Outside paragraph</p>
+<p>Outside paragraph</p>
 
-  <div class="sidebar">
-    <p>Sidebar paragraph</p>
-  </div>
+<div class="sidebar">
+  <p>Sidebar paragraph</p>
+</div>
 `, 0.75),
+  )
+
+  yield* waitUntil("thats-not-happening")
+  cursor().position([spaceX[6.25], spaceNY[1.75]]);
+  yield* all(
+    cursor().opacity(1, 0.75),
+    cursor().position([spaceX[5.25], spaceNY[2.75]], 0.75),
+  )
+
+  yield* waitUntil("space-between")
+  yield* all(
+    cursor().position([spaceNX[0.67] + 8, spaceNY[4.5] + 8], 0.75),
+    cssCode().selection(word(0, 4, 10), 0.75),
   )
 
   yield* waitUntil("s10-end");
