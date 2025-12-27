b7cc0bbdb8fd885b7b3f9e9a7b5c5db4b28b2471
Priyanshu Sharma <84620013+PriyanshuCK@users.noreply.github.com>
2025-12-27 10:24:05 +0530

    Complete s13

--- videos/css-selectors-explained-simply-for-beginners/src/project.ts
+++ videos/css-selectors-explained-simply-for-beginners/src/project.ts
@@ -15,8 +15,9 @@ import scene10b from "./scenes/scene10-selector-lists-space-vs-comma-b?scene"
 import scene11 from "./scenes/scene11-selector-lists-invalidity?scene"
 import scene12 from "./scenes/scene12-universal-selector-intro?scene"
 import scene13 from "./scenes/scene13-universal-selector-css-reset?scene"
+import scene14 from "./scenes/scene14-universal-selector-specificity?scene"
 
 export default makeProject({
-  scenes: [scene1, scene2, scene3, scene4, scene5, scene6, scene7, scene8, scene9, scene10, scene10b, scene11, scene12, scene13],
+  scenes: [scene1, scene2, scene3, scene4, scene5, scene6, scene7, scene8, scene9, scene10, scene10b, scene11, scene12, scene13, scene14],
   audio: audio1,
 });
--- videos/css-selectors-explained-simply-for-beginners/src/scenes/scene13-universal-selector-css-reset.tsx
+++ videos/css-selectors-explained-simply-for-beginners/src/scenes/scene13-universal-selector-css-reset.tsx
@@ -1,10 +1,14 @@
-import { Img, makeScene2D } from "@motion-canvas/2d";
+import { Code, Img, Layout, makeScene2D } from "@motion-canvas/2d";
 import colors from "../lib/colors";
-import { Cursor, ExtendedCircle, ExtendedRect, Grid } from "../nodes";
+import { CSSCode, Cursor, ExtendedCircle, ExtendedRect, Grid } from "../nodes";
 import { all, createRef, Direction, slideTransition, waitUntil } from "@motion-canvas/core";
 import { ExtendedTxt } from "../nodes/ExtendedTxt";
 import spaceX, { spaceNX, spaceNY, spaceY } from "../lib/space";
 import ua1Img from "../images/s13/ua1.png";
+import ua2zImg from "../images/s13/ua2z.png";
+import ua2hImg from "../images/s13/ua2h.png";
+import ua3zImg from "../images/s13/ua3z.png";
+import ua3hImg from "../images/s13/ua3h.png";
 
 export default makeScene2D(function*(view) {
   view.fontFamily('Geist');
@@ -102,6 +106,159 @@ export default makeScene2D(function*(view) {
     cursor().opacity(1, 0.75),
     cursor().position([spaceX[2.25], spaceY[1]], 0.75),
   )
+
+  yield* waitUntil("the-problem");
+  const ua2z = createRef<Img>();
+  const ua2h = createRef<Img>();
+  const ua3z = createRef<Img>();
+  const ua3h = createRef<Img>();
+  const firefox = createRef<ExtendedTxt>();
+  const chrome = createRef<ExtendedTxt>();
+  const rect2 = createRef<ExtendedRect>();
+  const rect3 = createRef<ExtendedRect>();
+  const mayDiffer = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <Img
+        ref={ua2z}
+        src={ua2zImg}
+        opacity={0}
+        scale={0}
+        position={[spaceNX[3], spaceNY[3.5]]}
+      />
+      <Img
+        ref={ua3z}
+        src={ua3zImg}
+        opacity={0}
+        scale={0}
+        position={[spaceNX[3], spaceY[1]]}
+        radius={16}
+      />
+      <Img
+        ref={ua2h}
+        src={ua2hImg}
+        opacity={0}
+        scale={0}
+        position={[spaceX[3], spaceNY[3.5]]}
+      />
+      <Img
+        ref={ua3h}
+        src={ua3hImg}
+        opacity={0}
+        scale={0}
+        position={[spaceX[3], spaceY[1]]}
+        radius={16}
+      />
+      <ExtendedTxt
+        ref={firefox}
+        text={"Firefox"}
+        position={[spaceNX[3], spaceNY[4.5]]}
+        fontSize={spaceY[0.5]}
+        opacity={0}
+      />
+      <ExtendedTxt
+        ref={chrome}
+        text={"Chrome"}
+        position={[spaceX[3], spaceNY[4.5]]}
+        fontSize={spaceY[0.5]}
+        opacity={0}
+      />
+      <ExtendedRect
+        ref={rect2}
+        size={[spaceX[2.5] - 8, spaceY[0.33] + 8]}
+        position={[spaceNX[3.75] - 4, spaceNY[0.25] + 8]}
+        lineWidth={6}
+        opacity={0}
+        scale={0.5}
+      />
+      <ExtendedRect
+        ref={rect3}
+        size={[spaceX[2.5] - 8, spaceY[0.33]]}
+        position={[spaceX[2.75], spaceY[3.75] + 4]}
+        lineWidth={6}
+        opacity={0}
+        scale={0.5}
+      />
+      <ExtendedTxt
+        ref={mayDiffer}
+        y={spaceY[5.25]}
+      />
+
+    </>
+  )
+
+  yield* all(
+    ua1().opacity(0, 0.75),
+    ua1().scale(0.3, 0.75),
+    rect1().opacity(0, 0.75),
+    rect1().scale(0.5, 0.75),
+    circle1().opacity(0, 0.75),
+    circle1().scale(0.5, 0.75),
+    cursor().opacity(0, 0.75),
+    cursor().position([spaceX[1.25], spaceY[2]], 0.75),
+    ua2z().opacity(1, 0.75),
+    ua2z().scale(0.5, 0.75),
+    ua3z().opacity(1, 0.75),
+    ua3z().scale(0.4, 0.75),
+    ua2h().opacity(1, 0.75),
+    ua2h().scale(0.5, 0.75),
+    ua3h().opacity(1, 0.75),
+    ua3h().scale(0.4, 0.75),
+    firefox().opacity(1, 0.75),
+    chrome().opacity(1, 0.75),
+  )
+
+  yield* waitUntil("not-the-same");
+  yield* all(
+    rect2().opacity(1, 0.75),
+    rect2().scale(1, 0.75),
+    rect3().opacity(1, 0.75),
+    rect3().scale(1, 0.75),
+    mayDiffer().text("*UA styles may differ", 1.25),
+  )
+
+  yield* waitUntil("this-rule");
+  const css = createRef<Code>();
+  const min = createRef<ExtendedTxt>();
+  view.add(
+    <>
+      <ExtendedTxt
+        ref={min}
+        fontSize={spaceY[0.5]}
+        y={spaceNY[3.5]}
+      />
+      <CSSCode
+        ref={css}
+        code={`\
+* {
+  margin: 0;
+  padding: 0;
+}
+`}
+        opacity={0}
+        y={spaceY[1]}
+        fontSize={spaceY[0.5]}
+      />
+    </>
+  )
+
+  yield* all(
+    css().opacity(1, 0.75),
+    css().y(0, 0.75),
+    rect2().opacity(0, 0.75),
+    rect3().opacity(0, 0.75),
+    rect2().scale(0.5, 0.75),
+    rect3().scale(0.5, 0.75),
+    mayDiffer().opacity(0, 0.75),
+    ua2z().opacity(0, 0.75),
+    ua3z().opacity(0, 0.75),
+    ua2h().opacity(0, 0.75),
+    ua3h().opacity(0, 0.75),
+    firefox().opacity(0, 0.75),
+    chrome().opacity(0, 0.75),
+  )
+
+  yield* min().text("Minimal CSS Reset", 1.25);
 
   yield* waitUntil("s13-end");
 
